41d00113be7d90cfe389872db2e0a857513c771f
min-hyo-2k <mrrobotforwork@gmail.com>
2024-02-26 16:08:20 +0700

    edit main tf

--- AWS-Terraform/main.tf
+++ AWS-Terraform/main.tf
@@ -1,19 +1,16 @@
 # VPC
 resource "aws_vpc" "vpc_demo" {
-  count = 1
   cidr_block = "10.0.0.0/16"
 }
 
 # subnet
 resource "aws_subnet" "subnet_demo" {
-  count = 1
   vpc_id = aws_vpc.vpc_demo.id
   cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_internet_gateway" "demo_gw" {
-  count = 1
- vpc_id = aws_vpc.vpc_demo.id
+  vpc_id = aws_vpc.vpc_demo.id
  
  tags = {
    Name = "Demo Gateway"
@@ -21,7 +18,6 @@ resource "aws_internet_gateway" "demo_gw" {
 }
 
 resource "aws_route_table" "demo_rtb" {
-  count = 1
  vpc_id = aws_vpc.vpc_demo.id
  route {
    cidr_block = "0.0.0.0/0"
@@ -34,14 +30,12 @@ resource "aws_route_table" "demo_rtb" {
 }
 
 resource "aws_route_table_association" "demo_subnet_asso" {
- count = 1
  subnet_id      = aws_subnet.subnet_demo.id
  route_table_id = aws_route_table.demo_rtb.id
 }
 
 # EC2 instance
 resource "aws_instance" "ec2_demo" {
-  count = 1
   ami = "ami-0eb4694aa6f249c52"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet_demo.id
