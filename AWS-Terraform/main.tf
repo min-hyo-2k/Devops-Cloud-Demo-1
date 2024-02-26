bd6b227a9723d7cdba99380422188cee2c6b5137
min-hyo-2k <mrrobotforwork@gmail.com>
2024-02-26 23:44:59 +0700

    add ssh to sg

--- AWS-Terraform/main.tf
+++ AWS-Terraform/main.tf
@@ -57,6 +57,13 @@ resource "aws_security_group" "demo_sg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # For demonstration purposes, restrict this in production
   }
+  #ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port = 0
